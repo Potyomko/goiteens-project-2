64dd29e3f40a93f01e725e33914796b3851b6804
Oleh Tsap <olehtsap07@gmail.com>
2023-01-16 13:14:08 +0200

    додав файл з своєю роботую у форматі js

---
+++ src/index-js/calculate-hour-or-minutes.js
@@ -0,0 +1,8 @@
+// const calculateHourOrMinutes = prompt('Write the number');
+// const calculate = calculateHourOrMinutes / 60;
+// alert(calculate)
+
+// const calculate = function () {
+//     const calculation = calculateHourOrMinutes / 60
+//     return alert()
+// }
