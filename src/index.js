178e737af1994998c642c2bb02d66a2940be5b94
NNASTYA2007 <nedyaknast@gmail.com>
2023-01-29 17:45:28 +0100

    commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 // import './js/header-modal-online';
 // import './js/header-modal-offline';
-import './js/switcher';
+// import './js/switcher';
 import './js/cards';
 // import './js/ball-in-field';
 // import './js/calculate-hour-or-minutes';
-import './js/game-stone-scissors--paper';
+// import './js/game-stone-scissors--paper';
 // import './js/leap-year-or-no';
 // import './js/dino';
+import './js/memory-game';
