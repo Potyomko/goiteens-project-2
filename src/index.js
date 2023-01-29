ffaae761e600bd71587c08a9d64dafe104b916fb
Mukola <koliunia2006@gmail.com>
2023-01-29 11:44:28 +0200

    dino game

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ import './js/ball-in-field';
 import './js/calculate-hour-or-minutes';
 import './js/game-stone-scissors--paper';
 import './js/leap-year-or-no';
+import './js/dino';
---
+++ src/js/dino.js
@@ -0,0 +1,40 @@
+const gameEl = document.querySelector('.game')
+const dinosaurRef = document.createElement("div")
+dinosaurRef.id = 'dino'
+
+const cactussRef = document.createElement("div")
+cactussRef.id = 'cactus'
+
+const firstRef = document.createElement("div")
+firstRef.id = 'first-cloud'
+
+const secondRef = document.createElement("div")
+secondRef.id = 'second-cloud'
+// ====================================================================================
+console.log(gameEl);
+gameEl.appendChild(dinosaurRef)
+dinosaurRef.after(cactussRef)
+cactussRef.after(firstRef)
+firstRef.after(secondRef)
+// ====================================================================================
+const dino = document.querySelector("#dino")
+const cactus = document.querySelector("#cactus")
+
+const occasion = document.addEventListener("keydown", function (e) {
+    jump()
+})
+const jump = function () {
+    if (dino.classList != "jump") {
+        dino.classList.add("jump")
+    }
+    setTimeout(function() {
+        dino.classList.remove("jump")
+        }, 300)
+}
+let isAlive = setInterval (function() {
+    const dinoUp = parseInt(window.getComputedStyle(dino).getPropertyValue("top"))
+    const cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"))
+    if (cactusLeft < 50 && cactusLeft > 0 && dinoUp >= 140) {
+        alert("GAME OVER")
+    }
+}, 10)
