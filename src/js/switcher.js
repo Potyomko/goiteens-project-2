8a173e5505c6dca83d0affc3934c87d36a27e98c
Olexander <cmurtimur@gmail.com>
2023-01-29 15:37:48 +0200

    додав чорну тему на модалку

--- src/js/switcher.js
+++ src/js/switcher.js
@@ -1,6 +1,8 @@
 const switcher = document.querySelector('.js-switch' )
 const headerEl = document.querySelector('.header-nav' )
 const buttonEl = document.querySelector('.button-log-in' )
+const modalOffline = document.querySelector('.form-offline')
+const modalOnline = document.querySelector('.form-online')
 switcher.addEventListener('change', switcherChecked)
 
 
@@ -11,11 +13,16 @@ function switcherChecked () {
       
        headerEl.classList.add("black-header-button");
        buttonEl.classList.add("black-header-button");
+       modalOffline.classList.add("black-modal");
+       modalOnline.classList.add("black-modal");
     }
     else{
         document.body.classList.remove("body-black")
         
         headerEl.classList.remove("black-header-button");
         buttonEl.classList.remove("black-header-button");
+
+        modalOffline.classList.remove("black-modal");
+       modalOnline.classList.remove("black-modal");
     }
 }
