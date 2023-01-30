b4d117fe42123c84b784b245811bcf3f53bfcc98
Sophia <melnichenko.cofia07@gmail.com>
2023-01-30 19:29:45 +0200

    +style

--- src/js/games/leap-year-or-no.js
+++ src/js/games/leap-year-or-no.js
@@ -1,35 +1,37 @@
 const modal = document.getElementById('1');
 
 const countainerEl = document.createElement('div');
-const whatSouuldDo = document.createElement('p');
 const inputEl = document.createElement('input');
-const reviewEl = document.createElement('button');
+const verifiEl = document.createElement('button');
 const answerEl = document.createElement('p');
 
 let year = 0;
 
-inputEl.classList.add('leap-year');
-reviewEl.classList.add('leap-year-btn');
+countainerEl.classList.add('year-countainer')
+inputEl.classList.add('year-field');
+verifiEl.classList.add('verifi');
+answerEl.classList.add('answer');
 
-// countainerEl.id = '1';
-whatSouuldDo.textContent = 'Введіть рік';
-reviewEl.textContent = 'Перевірити';
+verifiEl.textContent = 'Verifi';
 
-countainerEl.append(whatSouuldDo)
+// modal.style.backgroundColor = "$darkHeaderAndFooterColor"
+
+inputEl.placeholder = 'Put a year';
+
 countainerEl.append(inputEl);
-countainerEl.append(reviewEl)
+countainerEl.append(verifiEl)
 countainerEl.append(answerEl)
 modal.append(countainerEl)
 
 
 inputEl.addEventListener('input',hendlerInputEvent )
-reviewEl.addEventListener('click', hendlerReviewYear);
+verifiEl.addEventListener('click', hendlerVerifiYear);
 
 function hendlerInputEvent(event) {
     return year = Number(event.currentTarget.value)
 }
 
-function hendlerReviewYear() {
+function hendlerVerifiYear() {
     if (year % 4) {
         answerEl.textContent = 'Це не високосний рік :0'
     } else {
