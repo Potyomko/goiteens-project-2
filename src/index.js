64b395317035e941c5b74dd9f9e30554dbc0309e
Mukola <koliunia2006@gmail.com>
2023-01-31 17:43:01 +0200

    dino

--- src/index.js
+++ src/index.js
@@ -6,7 +6,11 @@ import './js/games/ball-in-field';
 import './js/games/calculate-hour-or-minutes';
 import './js/games/leap-year-or-no';
 import './js/games/guess-the-number';
+<<<<<<< Updated upstream
 import './js/games/dino';
+=======
+import './js/games/dino'
+>>>>>>> Stashed changes
 // import './js/game-stone-scissors--paper';
 // import './js/leap-year-or-no';
 // import './js/dino';
