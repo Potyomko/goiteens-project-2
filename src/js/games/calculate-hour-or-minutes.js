d09962aa5a39aef7f55bb257426443e73c2e4dd6
Oleh Tsap <olehtsap07@gmail.com>
2023-01-31 17:51:43 +0200

    заміна 2 слухачів на одного

--- src/js/games/calculate-hour-or-minutes.js
+++ src/js/games/calculate-hour-or-minutes.js
@@ -1,39 +1,41 @@
 const modal = document.getElementById('8');
 modal.classList.add('hour-container');
 
+const formEl = document.createElement('form');
+
+const textEl = document.createElement('p');
+textEl.classList.add('text-result');
+
 const inputEl = document.createElement('input');
 inputEl.classList.add('js-calculate');
 inputEl.classList.add('user-number');
 inputEl.type = 'text';
 inputEl.placeholder = 'Введи кількість хвилин і не пожалієш, без смс і реєстрацій';
-containerEl.append(inputEl);
-modal.appendChild(inputEl);
+// modal.appendChild(inputEl);
 
 const enterEl = document.createElement('button');
 enterEl.classList.add('enter')
 enterEl.type = 'button';
 enterEl.textContent = 'Ризкни';
-containerEl.append(enterEl);
-modal.appendChild(enterEl);
+// modal.appendChild(enterEl);
+
+formEl.append(inputEl, enterEl, textEl);
+
+modal.appendChild(formEl);
+
+formEl.addEventListener('submit', calculate);
 
 const input = document.querySelector('.js-calculate');
-input.addEventListener('input', calculate);
+// input.addEventListener('input', calculate);
 
 let calculation = 0;
 
 function calculate (event) {
     const numbers = Number(event.currentTarget.value);
-    return calculation = numbers / 60;
-}
-
-enterEl.addEventListener('click', createTextInput);
-
-function createTextInput() {
-    const textEl = document.createElement('p');
-    textEl.classList.add('text-result');
-    containerEl.append(textEl);
-    modal.appendChild(textEl);
+    calculation = numbers / 60;
     textEl.textContent = calculation;
     textEl.textContent = '';
 }
-            
+
+// enterEl.addEventListener('click', createTextInput);
+        
